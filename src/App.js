10b2ca0f1d183946750d1ccecff66f895edc8067
sinan akyol <110792519+sinanakyol@users.noreply.github.com>
2022-12-08 23:56:08 +0300

    files changed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Users from "./components/Users";
 import About from "./components/About";
+import User from "./components/User";
 
 function App() {
   return (
@@ -27,15 +28,10 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/users" component={Users} />
+          <Route path="/user/:id" component={User} />
         </Switch>
       </div>
     </Router>
